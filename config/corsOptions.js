107f03ee62b97f6e9bd40b215d0bb899d8b2b117
DESKTOP-5LSL4LR\SiAmComE <siamcomdeveloper@gmail.com>
2023-08-09 18:34:40 -0700

    update corsOptions with methods and allowedHeaders

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,9 +1,12 @@
 const allowedOrigins = require('./allowedOrigins');
 
-const corsOptions = {
-    origin: ['https://real-estate-properties-ms-frontend.onrender.com'],
-    credentials:true,            //access-control-allow-credentials:true
+const corsOptions = {   
+    origin: 'https://real-estate-properties-ms-frontend.onrender.com', // use your actual domain name (or localhost), using * is not recommended
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true,
     optionSuccessStatus:200,
+
     // origin: (origin, callback) => {
     //     if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
     //         callback(null, true)
